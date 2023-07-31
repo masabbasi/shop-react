b25f12c021824b8ef624669d702b4f2466b25b09
Your Namemasabbasi <masoud.abbasi.71@gmail.com>
2023-07-31 12:25:59 +0330

    All

--- src/context/ProductsContextProvider.jsx
+++ src/context/ProductsContextProvider.jsx
@@ -12,7 +12,6 @@ const ProductsContextProvider = ({children}) => {
 			setProducts(await getProducts());
 		};
     fetchAPI();
-		// getProducts().then(response=>{setProducts(response.data)})
   }, []);
 
   return (
